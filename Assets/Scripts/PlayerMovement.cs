7698c810519a0fcae0def55a4f897448930e6b7c
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2020-01-22 12:36:36 +0200

    Basic player movement implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float m_Speed =10f;
+    
+    private float m_MovementInputValueForward;
+    private float m_MovementInputValueRight;
+
+    private Rigidbody m_Rigidbody;
+
+    private void Awake()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        m_MovementInputValueForward = Input.GetAxis("Vertical");
+        m_MovementInputValueRight = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 movementForward = transform.forward * m_MovementInputValueForward * m_Speed * Time.deltaTime;
+        Vector3 movementRight = transform.right * m_MovementInputValueRight * m_Speed * Time.deltaTime;
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movementForward + movementRight);
     }
 }
