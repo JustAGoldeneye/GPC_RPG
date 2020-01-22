b38f062eb9fb0caad94d7279afd7e66a4f4b9c47
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2020-01-22 14:49:17 +0200

    Started working on making camera rotate around player, current solution not working as intended

--- Assets/Scripts/MainCameraControl.cs
+++ Assets/Scripts/MainCameraControl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MainCameraControl : MonoBehaviour
 {
+    public float m_MoveSpeed = 6.666f;
     public float m_TurnSpeed = 200f;
     public float m_AngleX = 15f;
     public float m_AngleZ = 0f;
@@ -17,14 +18,23 @@ public class MainCameraControl : MonoBehaviour
     private void FixedUpdate()
     {
         Turn();
+        MoveWhileTurning();
     }
 
     private void Turn()
     {
         float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
 
-        Quaternion turnRotation = Quaternion.Euler(m_AngleX, transform.localEulerAngles.y + turn, m_AngleZ);
+        Quaternion rotation = Quaternion.Euler(m_AngleX, transform.localEulerAngles.y + turn, m_AngleZ);
 
-        transform.localRotation = turnRotation;
+        transform.localRotation = rotation;
+    }
+
+    private void MoveWhileTurning()
+    {
+        Vector3 movement = transform.right * m_TurnInputValue * m_MoveSpeed * Time.deltaTime;
+
+        transform.localPosition = transform.localPosition + movement;
+        //huono ratkaisu, kääntyy väärään suuntaan ja pakenee
     }
 }
