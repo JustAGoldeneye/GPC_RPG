7804e0bbcf876891c39e2176c9bda7c766d4a4f4
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2020-02-22 21:14:00 +0200

    Started implemented UI choice confirmation graphic.

--- Assets/Scripts/UIButtonControl.cs
+++ Assets/Scripts/UIButtonControl.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UIButtonControl : MonoBehaviour
-
 {
+    public Image m_ButtonPressedImage;
+
+    private Canvas m_Canvas;
+
+    private void Awake()
+    {
+        m_Canvas = GetComponentInParent<Canvas>();
+    }
+
     public void ShowSelection()
     {
+        Image m_ButtonPressedImageInstance = Image.Instantiate(m_ButtonPressedImage, Vector3.zero, Quaternion.identity, GameObject.FindGameObjectWithTag("UI").transform);
         // Luodaan jonkinlainen valintaa osoittava UI-objekti painikkeen suhteellisen
         // sijainnin kohdalle (anna julkisen muuttujan määritteeksi prefab).
         // Objekti tuhoaa itsensä määritellyn ajan päästä.
