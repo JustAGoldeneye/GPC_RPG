16ab23fe35d728fcda354473bf16117f58cda6a9
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2020-02-29 03:22:39 +0200

    Implemented enemy movement

---
+++ Assets/Scripts/BattleActions/EnemyAttackAction.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackAction : AttackAction
+{
+    public float m_ActivationDistance = 10f;
+}
--- Assets/Scripts/Movement/EnemyMovement.cs
+++ Assets/Scripts/Movement/EnemyMovement.cs
@@ -4,8 +4,66 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private void Update()
+    public float m_DetectionRange = 200f;
+    public float m_BattleStayStillRange = 10f;
+    public float m_Speed = 10f;
+    public float m_TurnSpeed = 3f;
+
+    public GameObject m_Player;
+
+    private bool m_Battling;
+
+    private Rigidbody m_Rigidbody;
+
+    private void Awake()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Battling = false;
+    }
+
+    private void FixedUpdate()
+    {
+        CheckPlayerInDetectionRange();
+
+        if (m_Battling) {
+
+            LookToPlayer();
+            
+            if (DistanceToPlayer() > m_BattleStayStillRange) {
+                Move();
+            }
+
+            // Contact attacking script from here ???
+        }
+    }
+
+    private float DistanceToPlayer()
+    {
+        return (transform.position - m_Player.transform.position).magnitude;
+    }
+
+    private void CheckPlayerInDetectionRange()
+    {
+        m_Battling = DistanceToPlayer() <= m_DetectionRange;
+    }
+
+    // Method LookToPlayer() adapted from https://answers.unity.com/questions/417920/how-to-make-a-rigidbody-rotate-towards-an-object-u.html
+    private void LookToPlayer()
+    {
+        Vector3 TargetDirection = m_Player.transform.position - transform.position;
+        Vector3 LocalTarget = transform.InverseTransformPoint(m_Player.transform.position);
+
+        float Angle = Mathf.Atan2(LocalTarget.x, LocalTarget.z) * Mathf.Rad2Deg;
+
+        Vector3 EulerAngleVelocity = new Vector3(0, Angle, 0);
+
+        Quaternion DeltaRoation = Quaternion.Euler(EulerAngleVelocity * m_TurnSpeed * Time.deltaTime);
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * DeltaRoation);
+    }
+
+    private void Move()
+    {
+        Vector3 movement = transform.forward * m_Speed * Time.deltaTime;
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
     }
 }
