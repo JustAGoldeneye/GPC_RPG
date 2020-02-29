235d4aa79819f9990e694c5259a259cb1456df7b
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2020-02-29 20:03:34 +0200

    Started working on enemy attacking, backup commit

--- Assets/Scripts/BattleActions/EnemyAttackAction.cs
+++ Assets/Scripts/BattleActions/EnemyAttackAction.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class EnemyAttackAction : AttackAction
 {
     public float m_ActivationDistance = 10f;
+
+    public void ExecuteIfInRange(float Distance)
+    {
+        if (Distance <= m_ActivationDistance)
+        {
+            Execute();
+        }
+    }
 }
---
+++ Assets/Scripts/EnemyActionControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyActionControl : MonoBehaviour
+{
+    public void ExecuteAttacksInRange(float Distance)
+    {
+        // Joku timer
+        GetComponent<EnemyAttack1Action>().ExecuteIfInRange(Distance);
+    }
+}
